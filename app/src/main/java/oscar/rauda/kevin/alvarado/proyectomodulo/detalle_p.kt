65e6bc0df5ec5cb5c94d4f7023fe653a05401312
Kevin <kevinjah1975@gmail.com>
2024-07-25 21:31:07 -0600

    cree class detalle

---
+++ app/src/main/java/oscar/rauda/kevin/alvarado/proyectomodulo/detalle_p.kt
@@ -0,0 +1,20 @@
+package oscar.rauda.kevin.alvarado.proyectomodulo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class detalle_p : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detalle_p)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
